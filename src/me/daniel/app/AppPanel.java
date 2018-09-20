8e1f3dd69a385dcc12e000209fecb64e963ff2e2
Daniel Kowalski <danielk.biznes@gmail.com>
2018-09-20 21:46:57 +0200

    Change layout to grid

--- src/me/daniel/app/AppPanel.java
+++ src/me/daniel/app/AppPanel.java
@@ -1,26 +1,65 @@
 package me.daniel.app;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import me.daniel.button.AppButton;
 import me.daniel.button.ClickCallback;
 
 class AppPanel extends JPanel {
     private static final long serialVersionUID = 1L;
-    private JTextField numberOfClicks, clicksPerMinute, pressedKeys;
+    private JTextArea numberOfClicks, clicksPerMinute, pressedKeys;
     
     AppPanel() {
-        setLayout(null);
+        setLayout(new GridLayout(7, 1));
         setBackground(Color.BLACK);
         
-        initLabels();
-        initTextFields();
+        initGuiElements();
         initButton();
         reset();
+    }
+    
+    private void initGuiElements() {
+        numberOfClicks = new JTextArea();
+        clicksPerMinute = new JTextArea();
+        pressedKeys = new JTextArea();
+        
+        addLabel("Kliknięcia myszką: ");
+        initTextArea(numberOfClicks);
+        addLabel("Kliknięcia na minutę: ");
+        initTextArea(clicksPerMinute);
+        addLabel("Przyciśnięte klawisze: ");
+        initTextArea(pressedKeys);
+    }
+
+    private void initTextArea(JTextArea textArea) {
+        textArea.setBackground(Color.BLACK);
+        textArea.setForeground(Color.YELLOW);
+        
+        JScrollPane scroll = new JScrollPane(textArea);
+        scroll.setBorder(null);
+        
+        add(scroll);
+    }
+
+    private void addLabel(String text) {
+        JLabel label = new JLabel(text);
+        
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setForeground(Color.YELLOW);
+        
+        add(label);
+    }
+    
+    private void reset() {
+        numberOfClicks.setText("0");
+        clicksPerMinute.setText("0");
+        pressedKeys.setText("");
     }
     
     private void initButton() {
@@ -31,62 +70,7 @@ class AppPanel extends JPanel {
                 reset();
             }
         });
-        
-        button.setSize(AppFrame.WIDTH / 4, AppFrame.HEIGHT / 6);
-        button.setLocation((AppFrame.WIDTH - button.getWidth()) / 2, 2 * AppFrame.HEIGHT / 3);
-        
+
         add(button);
-    }
-
-    private void initTextFields() {
-        numberOfClicks = new JTextField();
-        clicksPerMinute = new JTextField();
-        pressedKeys = new JTextField();
-        
-        addTextField(numberOfClicks, 0);
-        addTextField(clicksPerMinute, 1);
-        addTextField(pressedKeys, 2);
-    }
-
-    private void initLabels() {
-        addLabel("Kliknięcia myszką: ", 0);
-        addLabel("Kliknięcia na minutę: ", 1);
-        addLabel("Przyciśnięte klawisze: ", 2);
-    }
-
-    private void addTextField(JTextField textField, int order) {
-        Color backgroundColor = Color.BLACK;
-        Color textColor = Color.YELLOW;
-        int width = AppFrame.WIDTH - AppFrame.WIDTH / 50;
-        int height = AppFrame.HEIGHT / 10;
-        int x = AppFrame.WIDTH / 100;
-        int startY = AppFrame.HEIGHT / 100 + height;
-        
-        textField.setBounds(x, startY + height * 2 * order, width, height);
-        textField.setBackground(backgroundColor);
-        textField.setForeground(textColor);
-        
-        add(textField);
-    }
-    
-    private void addLabel(String text, int order) {
-        Color textColor = Color.YELLOW;
-        int x = AppFrame.WIDTH / 100;
-        int startY = AppFrame.HEIGHT / 100;
-        int width = AppFrame.WIDTH / 2;
-        int height = AppFrame.HEIGHT / 10;
-        
-        JLabel label = new JLabel(text);
-        
-        label.setBounds(x, startY + order * 2 * height, width, height);
-        label.setForeground(textColor);
-        
-        add(label);
-    }
-    
-    private void reset() {
-        numberOfClicks.setText("0");
-        clicksPerMinute.setText("0");
-        pressedKeys.setText("");
     }
 }
