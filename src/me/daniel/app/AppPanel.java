e80c3a01e9b689f64864864c21d6e25877756274
Daniel Kowalski <danielk.biznes@gmail.com>
2018-09-20 20:03:52 +0200

    Add labels to sign text fields

--- src/me/daniel/app/AppPanel.java
+++ src/me/daniel/app/AppPanel.java
@@ -2,6 +2,7 @@ package me.daniel.app;
 
 import java.awt.Color;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 class AppPanel extends JPanel {
@@ -10,5 +11,26 @@ class AppPanel extends JPanel {
     AppPanel() {
         setLayout(null);
         setBackground(Color.BLACK);
+        
+        initLabel();
+    }
+    
+    private void initLabel() {
+        addLabel("Kliknięcia myszką: ", 0);
+        addLabel("Kliknięcia na minutę: ", 1);
+        addLabel("Przyciśnięte klawisze: ", 2);
+    }
+
+    private void addLabel(String text, int order) {
+        Color textColor = Color.YELLOW;
+        int x = (int)(AppFrame.WIDTH / 100);
+        int startY = (int)(AppFrame.HEIGHT / 100);
+        int width = (int)(AppFrame.WIDTH / 2);
+        int height = (int)(AppFrame.HEIGHT / 10);
+        
+        JLabel label = new JLabel(text);
+        label.setBounds(x, startY + order * 2 * height, width, height);
+        label.setForeground(textColor);
+        add(label);
     }
 }
