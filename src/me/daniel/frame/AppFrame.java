d4fac15df1ad104548e83c7d40f105437364cddb
Daniel Kowalski <danielk.biznes@gmail.com>
2018-09-20 17:56:07 +0200

    Add app's frame.

---
+++ src/me/daniel/frame/AppFrame.java
@@ -0,0 +1,28 @@
+package me.daniel.frame;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AppFrame extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private static final int WIDTH = 300, HEIGHT = 300;
+    private static final String TITLE = "In background listener";
+    
+    public AppFrame() {
+        setSize(WIDTH, HEIGHT);
+        setTitle(TITLE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() {
+                new AppFrame();
+            }
+        });
+    }
+}
